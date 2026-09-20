c3b938f199e832ff0c80823ba85fc2b6c30e8e2e
derglasi-beep <der.glasi@googlemail.com>
2026-09-20 21:42:58 +0200

    feat: initialize media-kit audio backend

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:just_audio_media_kit/just_audio_media_kit.dart';
 import 'app.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
+  JustAudioMediaKit.ensureInitialized();
   runApp(const Mp3ByGlasiApp());
 }
