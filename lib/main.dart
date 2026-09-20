cabfaec0c24641ed83c13bf4ce750547effb2956
derglasi-beep <der.glasi@googlemail.com>
2026-09-20 22:07:01 +0200

    Initialize Android background audio service

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,31 @@
-import 'package:flutter/material.dart';
-import 'package:just_audio_media_kit/just_audio_media_kit.dart';
-import 'app.dart';
+import 'dart:io';
 
-void main() {
+import 'package:audio_service/audio_service.dart';
+import 'package:flutter/widgets.dart';
+import 'package:just_audio_media_kit/just_audio_media_kit.dart';
+
+import 'app.dart';
+import 'services/audio_handler.dart';
+import 'services/audio_player_service.dart';
+
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   JustAudioMediaKit.ensureInitialized();
-  runApp(const Mp3ByGlasiApp());
+
+  final player = AudioPlayerService();
+
+  if (Platform.isAndroid) {
+    await AudioService.init(
+      builder: () => GlasiAudioHandler(player),
+      config: const AudioServiceConfig(
+        androidNotificationChannelId: 'de.glasi.mp3byglasi.audio',
+        androidNotificationChannelName: 'MP3 by Glasi',
+        androidNotificationOngoing: true,
+        androidStopForegroundOnPause: false,
+        androidResumeOnClick: true,
+      ),
+    );
+  }
+
+  runApp(Mp3ByGlasiApp(player: player));
 }
