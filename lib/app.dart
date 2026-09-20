e09a10035f727fad0dcb4153773dcadf7665dd36
derglasi-beep <der.glasi@googlemail.com>
2026-09-20 22:06:50 +0200

    Wire app to shared audio player service

--- lib/app.dart
+++ lib/app.dart
@@ -1,22 +1,70 @@
 import 'package:flutter/material.dart';
+
 import 'services/audio_player_service.dart';
 import 'screens/player_screen.dart';
 import 'screens/settings_screen.dart';
 import 'theme/glasi_theme.dart';
 
-class Mp3ByGlasiApp extends StatefulWidget { const Mp3ByGlasiApp({super.key}); @override State<Mp3ByGlasiApp> createState()=>_AppState(); }
-class _AppState extends State<Mp3ByGlasiApp> {
-  late final AudioPlayerService player;
-  @override void initState(){super.initState();player=AudioPlayerService();}
-  @override void dispose(){player.dispose();super.dispose();}
-  @override Widget build(BuildContext context)=>MaterialApp(
-    title:'MP3 by Glasi',debugShowCheckedModeBanner:false,
-    theme:GlasiTheme.dark(),
-    home: _Shell(player:player),
-  );
+class Mp3ByGlasiApp extends StatefulWidget {
+  final AudioPlayerService player;
+
+  const Mp3ByGlasiApp({super.key, required this.player});
+
+  @override
+  State<Mp3ByGlasiApp> createState() => _AppState();
 }
 
-class _Shell extends StatefulWidget { final AudioPlayerService player; const _Shell({required this.player}); @override State<_Shell> createState()=>_ShellState(); }
-class _ShellState extends State<_Shell> { int index=0;
-  @override Widget build(BuildContext context){ final pages=[PlayerScreen(player:widget.player),SettingsScreen(player:widget.player)]; return Scaffold(body:pages[index],bottomNavigationBar:NavigationBar(selectedIndex:index,onDestinationSelected:(v)=>setState(()=>index=v),destinations:const[NavigationDestination(icon:Icon(Icons.library_music),label:'Player'),NavigationDestination(icon:Icon(Icons.tune),label:'Audio')]),); }
+class _AppState extends State<Mp3ByGlasiApp> {
+  @override
+  void dispose() {
+    widget.player.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) => MaterialApp(
+        title: 'MP3 by Glasi',
+        debugShowCheckedModeBanner: false,
+        theme: GlasiTheme.dark(),
+        home: _Shell(player: widget.player),
+      );
+}
+
+class _Shell extends StatefulWidget {
+  final AudioPlayerService player;
+
+  const _Shell({required this.player});
+
+  @override
+  State<_Shell> createState() => _ShellState();
+}
+
+class _ShellState extends State<_Shell> {
+  int index = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    final pages = [
+      PlayerScreen(player: widget.player),
+      SettingsScreen(player: widget.player),
+    ];
+
+    return Scaffold(
+      body: pages[index],
+      bottomNavigationBar: NavigationBar(
+        selectedIndex: index,
+        onDestinationSelected: (value) => setState(() => index = value),
+        destinations: const [
+          NavigationDestination(
+            icon: Icon(Icons.library_music),
+            label: 'Player',
+          ),
+          NavigationDestination(
+            icon: Icon(Icons.tune),
+            label: 'Audio',
+          ),
+        ],
+      ),
+    );
+  }
 }
