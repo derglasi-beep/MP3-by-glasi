30600226f6b75b03d64a93720abda1473e3806de
derglasi-beep <der.glasi@googlemail.com>
2026-09-20 22:06:41 +0200

    Fix audio handler imports

--- lib/services/audio_handler.dart
+++ lib/services/audio_handler.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 
 import 'package:audio_service/audio_service.dart';
+import 'package:just_audio/just_audio.dart';
 
 import '../models/track.dart';
 import 'audio_player_service.dart';
